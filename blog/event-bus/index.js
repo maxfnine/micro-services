1920d32aab84a8d1e963b648b5ed8ccd286f6285
unknown <maxfnine@gmail.com>
2020-05-29 21:25:09 +0300

    Adding event storage functionality

--- blog/event-bus/index.js
+++ blog/event-bus/index.js
@@ -5,16 +5,25 @@ const axios = require('axios');
 const app = express();
 app.use(bodyParser.json());
 
-app.post('/events', (request, response) => {
-    const event = request.body;
-    axios.post('http://localhost:4000/events', event);
-    axios.post('http://localhost:4001/events', event);
-    axios.post('http://localhost:4002/events', event);
-    axios.post('http://localhost:4003/events', event);
+const events = [];
 
-    response.send({ status: 'OK' });
+app.post('/events', (req, res) => {
+  const event = req.body;
+
+  events.push(event);
+
+  axios.post('http://localhost:4000/events', event);
+  axios.post('http://localhost:4001/events', event);
+  axios.post('http://localhost:4002/events', event);
+  axios.post('http://localhost:4003/events', event);
+
+  res.send({ status: 'OK' });
+});
+
+app.get('/events', (req, res) => {
+  res.send(events);
 });
 
 app.listen(4005, () => {
-    console.log("Event bus listening on 4005");
+  console.log('Listening on 4005');
 });
--- blog/query/index.js
+++ blog/query/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
 app.use(bodyParser.json());
@@ -8,38 +9,53 @@ app.use(cors());
 
 const posts = {};
 
-app.get('/posts',(request,response)=>{
-    response.send(posts);
+const handleEvent = (type, data) => {
+  if (type === 'PostCreated') {
+    const { id, title } = data;
+
+    posts[id] = { id, title, comments: [] };
+  }
+
+  if (type === 'CommentCreated') {
+    const { id, content, postId, status } = data;
+
+    const post = posts[postId];
+    post.comments.push({ id, content, status });
+  }
+
+  if (type === 'CommentUpdated') {
+    const { id, content, postId, status } = data;
+
+    const post = posts[postId];
+    const comment = post.comments.find(comment => {
+      return comment.id === id;
+    });
+
+    comment.status = status;
+    comment.content = content;
+  }
+};
+
+app.get('/posts', (req, res) => {
+  res.send(posts);
 });
 
-app.post('/events',(request,response)=>{
-    const {type,data} = request.body;
+app.post('/events', (req, res) => {
+  const { type, data } = req.body;
 
-    if(type === 'PostCreated'){
-        const {id,title} = data;
-        posts[id]={id,title};
-    }
+  handleEvent(type, data);
 
-    if(type === 'CommentCreated'){
-        const {id,content,postId,status} = data;
-        post = posts[postId];
-        post.comments = post.comments || [];
-        post.comments.push({id,content,status});
-    }
-
-    if(type === 'CommentUpdated'){
-        const {id,content,postId,status} = data;
-        post = posts[postId];
-        const comment = post.comments.find(comment=>comment.id===id);
-        comment.id = id;
-        comment.content = content;
-        comment.postId = postId;
-        comment.status = status;
-    }
-
-    response.send({});
+  res.send({});
 });
 
-app.listen(4002, () => {
-    console.log("Query server started on port 4002!");
+app.listen(4002, async () => {
+  console.log('Listening on 4002');
+
+  const res = await axios.get('http://localhost:4005/events');
+
+  for (let event of res.data) {
+    console.log('Processing event:', event.type);
+
+    handleEvent(event.type, event.data);
+  }
 });
