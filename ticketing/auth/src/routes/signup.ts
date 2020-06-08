341fcd1157d62ffecaa027f6caa8c7f49762905b
unknown <maxfnine@gmail.com>
2020-06-08 16:54:13 +0300

    extracted errors and middlewares from auth to common module

--- ticketing/auth/src/routes/signup.ts
+++ ticketing/auth/src/routes/signup.ts
@@ -1,8 +1,7 @@
 import express, { Request, Response } from "express";
 import { body, validationResult } from "express-validator";
 import jwt from "jsonwebtoken";
-import { validateRequest } from "../middlewares/validate-request";
-import { BadRequestError } from "../errors/bad-request-error";
+import { validateRequest, BadRequestError } from "@mftickets/common";
 import { User } from "../models/user";
 
 const router = express.Router();
